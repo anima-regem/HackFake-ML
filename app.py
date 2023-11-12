1aed6b38a1e0907b03f78033f98cace8926f7ce8
anima-regem <vichukartha@gmail.com>
2023-11-12 08:09:52 +0530

    corrects server url

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ async def predict(input_data: InputData):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
