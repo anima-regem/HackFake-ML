866b9ff5676f2ce93c5d3a2fa6a4bc1bb9f9b7fd
anima-regem <vichukartha@gmail.com>
2023-11-12 08:24:49 +0530

    logs steps

--- test.py
+++ test.py
@@ -3,20 +3,26 @@ from sentence_transformers import SentenceTransformer
 
 embedding_model = SentenceTransformer('paraphrase-multilingual-mpnet-base-v2')
 model = load_model("model.keras")
+print("Model loaded")
+print(model.summary())
 
 def predict(input_text):
     # input_encoding = tokenizer([input_text], truncation=True, padding=True, max_length=max_length, return_tensors='tf')
     input_embedding = embedding_model.encode([input_text])
+    print("Input embedding: ", input_embedding)
 
     predictions = model.predict(input_embedding)
+    print("Predictions: ", predictions)
 
     predicted_probabilities = predictions[0]
+    print("Predicted probabilities: ", predicted_probabilities)
 
     class_labels = ['Hate', 'Misleading', 'Disinformation', 'Rumour', 'Sensationalism']
 
     confidence_levels = {}
     for label, prob in zip(class_labels, predicted_probabilities):
         confidence_levels[label] = prob
+        print(f"{label}: {prob}")
     
     return confidence_levels
 
